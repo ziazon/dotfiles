f740685c2226cc7ad5d2f4cc669a22c2ea03bb1a
Jubair Saidi <jubairs@known.is>
2020-09-30 22:52:59 -0400

    broke installs up to be individually executed so errors on one install doesn't break all the rest.
    adds some missing setup commands.
    adds poetry auto complete

--- install.sh
+++ install.sh
@@ -14,81 +14,79 @@ brew tap homebrew/cask-fonts
 
 brew install svn
 
-brew cask install \
-docker \
-font-fira-code \
-font-source-code-pro-for-powerline \
-google-chrome \
-iterm2 \
-karabiner-elements \
-rectangle \
-virtualbox \
-xquartz
+brew cask install docker
+brew cask install font-fira-code
+brew cask install font-source-code-pro-for-powerline
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install karabiner-elements
+brew cask install rectangle
+brew cask install virtualbox
+brew cask install xquartz
 
-brew install \
-circleci \
-cmake \
-cookiecutter \
-ctags \
-direnv \
-docker-compose \
-findutils \
-fzf \
-fzy \
-gcc \
-gd \
-gh \
-git \
-git-delta \
-git-flow \
-git-lfs \
-glib \
-gmp \
-gnupg \
-gnutls \
-golang \
-graphviz \
-grep \
-gts \
-helm \
-httpie \
-jq \
-kops \
-kubectx \
-kubernetes-cli \
-kubernetes-helm \
-kubetail \
-libpq \
-libressl \
-lua \
-minikube \
-neovim \
-newman \
-openjdk \
-pgcli \
-pipx \
-pyenv \
-pyenv-virtualenv \
-readline \
-rlwrap \
-ruby \
-shellcheck \
-shfmt \
-sphinx-doc \
-starship \
-terraform \
-tmux \
-unixodbc \
-vim \
-watch \
-webp \
-xclip \
-xz \
-yarn \
-zlib \
-zplug \
-zsh \
-zsh-completions
+brew install circleci
+brew install cmake
+brew install cookiecutter
+brew install ctags
+brew install direnv
+brew install docker-compose
+brew install findutils
+brew install fzf
+brew install fzy
+brew install gcc
+brew install gd
+brew install gh
+brew install git
+brew install git-delta
+brew install git-flow
+brew install git-lfs
+brew install glib
+brew install gmp
+brew install gnupg
+brew install gnutls
+brew install golang
+brew install graphviz
+brew install grep
+brew install gts
+brew install helm
+brew install httpie
+brew install jq
+brew install kops
+brew install kubectx
+brew install kubernetes-cli
+brew install kubernetes-helm
+brew install kubetail
+brew install libpq
+brew install libressl
+brew install lua
+brew install minikube
+brew install neovim
+brew install newman
+brew install openjdk
+brew install pgcli
+brew install pipx
+brew install pyenv
+brew install pyenv-virtualenv
+brew install readline
+brew install rlwrap
+brew install ruby
+brew install shellcheck
+brew install shfmt
+brew install sphinx-doc
+brew install starship
+brew install terraform
+brew install tmux
+brew install unixodbc
+brew install vim
+brew install watch
+brew install webp
+brew install xclip
+brew install xz
+brew install yarn
+brew install zlib
+brew install zplug
+brew install zsh
+brew install zsh-completions
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
 
@@ -99,30 +97,28 @@ export NVM_DIR="$HOME/.nvm"
 
 nvm install v12.18.4 # use nvm ls-remote to get current version first
 
-npm i -g \
-@nestjs/cli \
-@vue/cli \
-aws-cdk \
-eslint \
-gulp \
-init-module \
-madge \
-npm-check-updates \
-pug-lint \
-pyright \
-sort-package-json \
-typeorm \
-typesync \
-vue-language-server
+npm i -g @nestjs/cli
+npm i -g @vue/cli
+npm i -g aws-cdk
+npm i -g eslint
+npm i -g gulp
+npm i -g init-module
+npm i -g madge
+npm i -g npm-check-updates
+npm i -g pug-lint
+npm i -g pyright
+npm i -g sort-package-json
+npm i -g typeorm
+npm i -g typesync
+npm i -g vue-language-server
 
 # install cli packages in rust
-cargo install \
-bat \
-du-dust \
-exa \
-prettydiff \
-procs \
-ripgrep
+cargo install bat
+cargo install du-dust
+cargo install exa
+cargo install prettydiff
+cargo install procs
+cargo install ripgrep
 
 # install Zinit for zsh plugin managmenet
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
@@ -133,12 +129,13 @@ curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg" && sudo insta
 rm -f AWSCLIV2.pkg
 
 # install services
-brew install nginx \
-postgresql \
-rabbitmq \
-redis \
-mysql
+brew install nginx
+brew install postgresql
+brew install rabbitmq
+brew install redis
+brew install mysql
 
+compaudit | xargs chmod g-w
 
 UPDATEZSHRC=1
 
@@ -168,6 +165,17 @@ cat $HOME/.env/starship.toml > $HOME/.config/starship.toml
 
 echo "\033[38;5;111mLoading changes to $HOME/.zshrc to shell\033[0m"
 zsh $HOME/.zshrc
+
+echo "\033[38;5;111mInstall python 3.7.8\033[0m"
+pyenv install 3.7.8
+
+echo "\033[38;5;111mSet python 3.7.8 as system default\033[0m"
+pyenv global 3.7.8
+
+
+echo "\033[38;5;111minstall git-lfs\033[0m"
+git lfs install
+git lfs install --system
 
 echo "\033[38;5;111mSet poetry virtualenvs to be stored in project\033[0m"
 poetry config virtualenvs.in-project true
